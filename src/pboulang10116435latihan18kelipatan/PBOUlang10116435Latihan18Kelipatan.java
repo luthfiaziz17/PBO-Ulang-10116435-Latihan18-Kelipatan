05f3286c5c30987e2a38c95c99fdf790e44caf69
nanda <nanda@nanda.local>
2019-10-10 14:37:22 +0700

    commit men

---
+++ src/pboulang10116435latihan18kelipatan/PBOUlang10116435Latihan18Kelipatan.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pboulang10116435latihan18kelipatan;
+
+/**
+ *
+ * @author Luthfi
+ * NIM  : 10116435
+ * NAMA : Luthfi Aziz Al Paqih
+ */
+
+public class PBOUlang10116435Latihan18Kelipatan {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+          double i=3.5;
+        do{
+            if (i >= 3.5) {
+                System.out.println(i + "");
+            }
+             i +=3.5;
+        }
+        while (i <=35); {
+    }
+    }
+    
+}
